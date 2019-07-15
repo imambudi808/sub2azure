aaaea058d7c95295eedd6bb0dad78b6fe2c1d4ac
m.imam budi laksamana <imambudi808@gmail.com>
2019-07-15 14:51:34 +0700

    sub2azure

---
+++ computervision.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="description" content="">
+            <meta name="author" content="">
+            
+
+            <title>Undip Smart Parkir</title>
+
+            <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+            
+
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    </head>
+        <body>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		
+		</nav>
+		<main role="main" class="container">
+    		
+        <script type="text/javascript">
+            $(document).ready(function () {
+            // **********************************************
+            // *** Update or verify the following values. ***
+            // **********************************************
+            // Replace <Subscription Key> with your valid subscription key.
+            var subscriptionKey = "86e86adca3e949debada81e072b77cb7";
+            // You must use the same Azure region in your REST API method as you used to
+            // get your subscription keys. For example, if you got your subscription keys
+            // from the West US region, replace "westcentralus" in the URL
+            // below with "westus".
+            //
+            // Free trial subscription keys are generated in the "westus" region.
+            // If you use a free trial subscription key, you shouldn't need to change
+            // this region.
+            var uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+            // Request parameters.
+            var params = {
+                "visualFeatures": "Categories,Description,Color",
+                "details": "",
+                "language": "en",
+            };
+            // Display the image.
+            var sourceImageUrl = "<?php echo $url ?>";
+            document.querySelector("#sourceImage").src = sourceImageUrl;
+            // Make the REST API call.
+            $.ajax({
+                url: uriBase + "?" + $.param(params),
+                // Request headers.
+                beforeSend: function(xhrObj){
+                    xhrObj.setRequestHeader("Content-Type","application/json");
+                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+                },
+                type: "POST",
+                // Request body.
+                data: '{"url": ' + '"' + sourceImageUrl + '"}',
+            })
+            .done(function(data) {
+                // Show formatted JSON on webpage.
+                $("#responseTextArea").val(JSON.stringify(data, null, 2));
+                // console.log(data);
+                // var json = $.parseJSON(data);
+                $("#description").text(data.description.captions[0].text);
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                // Display error message.
+                var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+                errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+                alert(errorString);
+            });
+        });
+    </script>
+<br>
+<div id="wrapper" style="width:1020px; display:table;">
+	<div id="jsonOutput" style="width:600px; display:table-cell;">
+		<b>Response:</b>
+		<br><br>
+		<textarea id="responseTextArea" class="UIInput"
+		style="width:580px; height:400px;" readonly=""></textarea>
+	</div>
+	<div id="imageDiv" style="width:420px; display:table-cell;">
+		<b>Source Image:</b>
+		<br><br>
+		<img id="sourceImage" width="400" />
+		<br>
+		<h3 id="description">Loading . . .</h3>
+	</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "./random_string.php";
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=sub2contoh;AccountKey=WQCVKlwYw5SPuRkeKxeEiJKByVrMiRhddBukZE9pb/KMvN7eREaodjC8niDaKEFtOhKft2kKxm23NmV7emh+bA==;";
+$containerName = "sub2contoh";
+// Create blob client.
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+if (isset($_POST['submit'])) {
+	$fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
+	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
+	// echo fread($content, filesize($fileToUpload));
+	$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+	header("Location: index.php");
+}
+$listBlobsOptions = new ListBlobsOptions();
+$listBlobsOptions->setPrefix("");
+$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+
+    <title>Submission2</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+  </head>
+<body>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+		
+		
+		</nav>
+		<main role="main" class="container">    		
+		<div class="mt-4 mb-2">
+			<form class="d-flex justify-content-lefr" action="index.php" method="post" enctype="multipart/form-data">
+				<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
+				<input type="submit" name="submit" value="Upload">
+			</form>
+		</div>
+		<br>
+		<br>
+		<h4>Total Files : <?php echo sizeof($result->getBlobs())?></h4>
+		<table class='table table-hover'>
+			<thead>
+				<tr>
+					<th>File Name</th>
+					<th>File URL</th>
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				do {
+					foreach ($result->getBlobs() as $blob)
+					{
+						?>
+						<tr>
+							<td><?php echo $blob->getName() ?></td>
+							<td><?php echo $blob->getUrl() ?></td>
+							<td>
+								<form action="computervision.php" method="post">
+									<input type="hidden" name="url" value="<?php echo $blob->getUrl()?>">
+									<input type="submit" name="submit" value="analisis!" class="btn btn-primary">
+								</form>
+							</td>
+						</tr>
+						<?php
+					}
+					$listBlobsOptions->setContinuationToken($result->getContinuationToken());
+				} while($result->getContinuationToken());
+				?>
+			</tbody>
+		</table>
+
+	</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
